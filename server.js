10a4af168a664051b12805d994ce75b434ad8232
Prateek <prateek15jun@gmail.com>
2020-04-13 21:42:39 +0530

    update in push comments and rating

--- server.js
+++ server.js
@@ -4,15 +4,14 @@ const bodyParser=require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const cors = require("cors");
 require('dotenv/config');
-
 const port =process.env.PORT || 3000;
-
 var app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/views') );
 app.set('view engine', hbs);
+
 
 app.get('/', (req,res) => {
     res.render('index.hbs');
@@ -25,10 +24,44 @@ app.post('/search',(req,res)=>{
  app.post('/submit',(req,res)=>{
      console.log(req.body.rating);
      console.log(req.body.comment);
-      
+     const id=res.app.get('id');
+     const id1=id.toString();
+     const uri = process.env.MONGO_URI;
+    
+    MongoClient.connect(uri,{ useNewUrlParser: true }, (erro,client) => {
+        if (erro){
+            return console.log('Unable to connect', erro);
+        };
+
+        console.log('Connected sucessfully');
+        const db = client.db('Foodie');
+        let rate=req.body.rating;
+        let a= (Math.random()*2);
+        a=a.toPrecision(2);
+        rate=(parseFloat(rate)+a)/2;
+        let rate1=rate.toString();
+        var myquery = { id: id1 };
+        var newvalues = {$set: {rating : rate1}};
+        db.collection("restaurants").updateOne(myquery, newvalues, function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+          });
+        var newvalues = {$push: {comments : req.body.comment}};
+        db.collection("restaurants").updateOne(myquery, newvalues, function(err, res) {
+            if (err) throw err;
+            console.log("1 document updated");
+          });
+        
+        
+        client.close();
+        res.render('result.hbs');
+        });
+
     });
 app.get('/view',(req,res)=>{
      res.render('view.hbs');
+
+    
        
 });
 app.post('/show',(req,res)=>{
@@ -51,6 +84,7 @@ app.get('/send',(req,res)=>{
                 console.log('Unable to add the weather data', erro);
             }
             res.json(result[id]);
+            
         });
         client.close();
         });
